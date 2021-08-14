abd5091e47afcaf4aca6ec1c87ab83f7df320024
Jonathan <joninf95@gmail.com>
2021-08-14 13:37:57 -0300

    UP

---
+++ Lista Geometria/Lista Geometria/tad-coordenadas-master/coordenadas.c
@@ -0,0 +1,140 @@
+#include "coordenadas.h"
+#include <math.h>
+#include <stdio.h>
+
+/// multiplica um vetor por um escalar
+/// este é um exemplo entregue pronto pra você ;)
+vetor multiplicaPorEscalar(vetor v, escalar alpha)
+{
+    vetor resultado =
+    {
+        v.x * alpha,
+        v.y * alpha,
+        v.z * alpha,
+        v.w * alpha
+    };
+    return resultado;
+}
+
+vetor somaVetorComVetor(vetor v, vetor u)
+{
+    //TODO: implementar
+    v.x = v.x + u.x;
+    v.y = v.y + u.y;
+    v.z = v.z + u.z;
+    v.w = v.w + u.w;
+
+    vetor resultado = v;
+    return resultado;
+}
+
+vetor diferencaVetorComVetor(vetor v, vetor u)
+{
+    //TODO: implementar
+    v.x = v.x - u.x;
+    v.y = v.y - u.y;
+    v.z = v.z - u.z;
+    v.w = v.w - u.w;
+
+    vetor resultado = v;
+    return resultado;
+}
+
+vetor diferencaEntrePontos(ponto p, ponto q)
+{
+    //TODO: implementar
+    vetor resultado = {p.x - q.x, p.y - q.y, p.z - q.z, p.w - q.w};
+    return resultado;
+}
+
+ponto somaPontoComVetor(ponto p, vetor v)
+{
+    //TODO: implementar
+    p.x = p.x + v.x;
+    p.y = p.y + v.y;
+    p.z = p.z + v.z;
+    p.w = p.w + v.w;
+
+    ponto resultado = p;
+    return resultado;
+}
+
+escalar normaDoVetor(vetor v)
+{
+    //TODO: implementar
+
+    escalar resultado = sqrt(pow(v.x, 2) + pow(v.y, 2) + pow(v.z, 2));
+    return resultado;
+}
+
+vetor normalizado(vetor v)
+{
+    //TODO: implementar
+    vetor resultado = {v.x/normaDoVetor(v), v.y/normaDoVetor(v), v.z/normaDoVetor(v), v.w/normaDoVetor(v)};
+    return resultado;
+}
+
+escalar distanciaEntrePontos(ponto p, ponto q)
+{
+    //TODO: implementar
+    escalar resultado = sqrt(pow(q.x - p.x, 2) + pow(q.y - p.y, 2) + pow(q.z - p.z, 2));
+    return resultado;
+}
+
+escalar produtoEscalar(vetor v, vetor u)
+{
+    //TODO: implementar
+    escalar resultado = v.x*u.x + v.y*u.y + v.z*u.z + v.w*u.w;
+    return resultado;
+}
+
+vetor produtoVetorial(vetor v, vetor u)
+{
+    //TODO: implementar
+    // Produto vetorial só faz sentido em 3D
+    // Ignorar a componente "w" de "v" e "u"
+    // Como o resultado é um vetor, o "w" dele deve ser 0
+    double aux1, aux2, aux3;
+
+    aux1 = v.y*u.z - v.z*u.y;
+    aux2 = u.x*v.z - u.z*v.x;
+    aux3 = v.x*u.y - v.y*u.x;
+
+    v.x = aux1;
+    v.y = aux2;
+    v.z = aux3;
+    vetor resultado = v;
+    return resultado;
+}
+
+///
+/// Referências: http://localhost:8080/classes/geometry/#30
+escalar anguloEntreVetores(vetor v, vetor u)
+{
+    //TODO: implementar
+    escalar resultado = acos(produtoEscalar(v, u)/(normaDoVetor(v)*normaDoVetor(u)));
+    return resultado;
+}
+
+///
+/// Referências: http://localhost:8080/classes/geometry/#22
+ponto combinacaoAfim2Pontos(ponto p, ponto q, escalar alpha)
+{
+    //TODO: implementar
+    p.x = (1 - alpha)*p.x + alpha*q.x;
+    p.y = (1 - alpha)*p.y + alpha*q.y;
+    p.z = (1 - alpha)*p.z + alpha*q.z;
+    p.w = (1 - alpha)*p.w + alpha*q.w;
+
+    ponto resultado = p;
+    return resultado;
+}
+
+/// Imprime um vetor ou ponto no terminal
+/// Uso:
+///   vetor r = somaVetorComVetor(a, b);
+///   imprime("vetor r", r);
+void imprime(struct coordenadas c, char* nome)
+{
+    printf("%s = { %.2f, %.2f, %.2f, %.2f }\n", nome, c.x, c.y, c.z, c.w);
+}
---
+++ Lista Revisão/atividade3/main.cpp
@@ -0,0 +1,149 @@
+/**
+    @disciplina Computacao Grafica
+    @aluno Jonathan Douglas Diego Tavares
+    @matricula 201622040228
+    @data 26/08/2019
+**/
+
+/**
+    Funcionalidade extra: Geracao de matriz com controle de linhas e colunas via teclado
+
+    Pressionar letra l para aumentar o numero de lados
+    Pressionar letra k para diminuir o numero de lados
+    Pressionar letra r para aumentar o raio
+    Pressionar letra e para diminuir o raio
+**/
+
+#include <GL/glew.h>      // glew.h deve vir antes
+#include <GL/freeglut.h>  // do freeglut.h
+#include <math.h>
+#include<stdio.h>
+
+//#define M_PI acos(-1.0)
+
+float nLados = 100000;
+float raio = 0.1;
+double angulo;
+
+void escreveTexto(void * font, char *s, float x, float y, float z) {
+    int i;
+    glRasterPos3f(x, y, z);
+
+    for (i = 0; i < strlen(s); i++) {
+        glutBitmapCharacter(font, s[i]);
+    }
+}
+
+void desenhaFigura(float x, float y){
+    glBegin(GL_LINE_STRIP);
+        for (float i = 0; i < nLados; i = i + 0.05){
+            glVertex3f(x+raio*cos(i),y+raio*sin(i), 0);
+            raio = raio + 1;
+        }
+    glEnd();
+}
+
+void desenhaHUD(){
+    char bufferRaio[20];
+    char bufferLados[20];
+    itoa(raio, bufferRaio, 10);
+    itoa(nLados, bufferLados, 10);
+
+    escreveTexto(GLUT_BITMAP_HELVETICA_12, "Raio:", 20, 580, 0);
+    escreveTexto(GLUT_BITMAP_HELVETICA_12, "nLados:", 20, 560, 0);
+    escreveTexto(GLUT_BITMAP_HELVETICA_12, bufferRaio, 50, 580, 0);
+    escreveTexto(GLUT_BITMAP_HELVETICA_12, bufferLados, 65, 560, 0);
+}
+
+// callback de desenho (display)
+void desenhaMinhaCena() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(0, 0, 0);
+    desenhaFigura(0, 0);
+    glFlush();
+}
+
+// NOVIDADE: uma função que vamos chamar dentro
+//    do "main"
+// Inicia algumas variáveis de estado do OpenGL
+void inicializa() {
+    glPointSize(2);
+    // define qual é a cor do fundo
+    glClearColor(1, 1, 1, 1); // branco
+    // desenho preenchido vs. contorno
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+}
+
+// NOVIDADE: callback para o evento "reshape"
+void redimensionada(int width, int height) {
+   glViewport(0, 0, width, height);
+
+   glMatrixMode(GL_PROJECTION);
+   glLoadIdentity();
+   glOrtho(-600, 600, -600, 600, -1, 1);
+
+   glMatrixMode(GL_MODELVIEW);
+   glLoadIdentity();
+}
+
+// NOVIDADE: callback de "keyboard"
+void teclaPressionada(unsigned char key, int x, int y) {
+    // vê qual tecla foi pressionada
+    switch(key) {
+    case 27:      // Tecla "ESC"
+        exit(0);  // Sai da aplicação
+        break;
+    case 'r':
+        raio = raio + 1;
+        glutPostRedisplay();
+        break;
+    case 'e':
+        if (raio > 1){
+            raio = raio - 1;
+        } else {
+            printf("Raio minimo atingido.\n");
+        }
+        glutPostRedisplay();
+        break;
+    case 'l':
+        nLados = nLados + 1;
+        glutPostRedisplay();
+        break;
+    case 'k':
+        if (nLados > 2){
+            nLados = nLados - 1;
+        } else {
+            printf("Minimo de lados atingido.\n");
+        }
+        glutPostRedisplay();
+        break;
+    default:
+        break;
+    }
+}
+
+// função principal
+int main(int argc, char** argv) {
+   glutInit(&argc, argv);
+
+   glutInitContextVersion(2, 1);
+   glutInitContextProfile(GLUT_COMPATIBILITY_PROFILE);
+
+   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+   glutInitWindowSize(800, 600);
+   glutInitWindowPosition(300, 100);
+
+   glutCreateWindow("Atividade 3 - Jonathan Douglas Diego Tavares - Matrícula: 201622040228");
+
+   // registra callbacks para alguns eventos
+   glutDisplayFunc(desenhaMinhaCena);
+   glutReshapeFunc(redimensionada);
+   glutKeyboardFunc(teclaPressionada);
+
+   // configura valor inicial de algumas
+   // variáveis de estado do OpenGL
+   inicializa();
+
+   glutMainLoop();
+   return 0;
+}
